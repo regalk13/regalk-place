bc36cf8707fe2741a4f0c5c1eddfe00fe6895a45
regalk <jsalazarcaviedes30@gmail.com>
2022-04-13 13:43:39 -0500

    :sparkles: End for now!

--- build-site.el
+++ build-site.el
@@ -1,5 +1,27 @@
+;; emacs config
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+
+;; init package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+
+;; Install dependencies :)
+(package-install 'htmlize)
+
 ;; Load the publishing system
 (require 'ox-publish)
+
+;; Customize the HTML output
+(setq org-html-validation-link nil              ;; Don't show validation link
+      org-html-head-include-scripts nil         ;; Use own scripts
+      org-html-head-include-default-style nil   ;; Use own styles
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
@@ -17,5 +39,3 @@
 
 ;; Generate the site output
 (org-publish-all t)
-
-(message "Build complete!")
