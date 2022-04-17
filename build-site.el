07a8df5b9c460ce7c1a9002dfd6072f5dcdfa5a4
regalk <jsalazarcaviedes30@gmail.com>
2022-04-17 16:22:38 -0500

    Checking emacs error...

--- build-site.el
+++ build-site.el
@@ -19,23 +19,25 @@
 ;; Customize the HTML output
 (setq org-html-validation-link nil              ;; Don't show validation link
       org-html-head-include-scripts nil         ;; Use own scripts
-      org-html-head-include-default-style nil)
+      org-html-head-include-default-style nil   ;; Use own styles
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
 
+;; Debug ;)
 ;; Define the publishing project
-(setq org-publish-project-alist
-      (list
-       (list "website-org-mode"
-             :recursive t
-             :base-directory "./content"
-             :publishing-function 'org-html-publish-to-html
-             :publishing-directory "./public"
-             :with-author nil                   ;; Don't show author
-             :with-creator t                    ;; Show emacs and org version
-             :with-toc nil                      ;; Don't show tables of content
-             :section-numbers nil               ;; No number section
-             :time-stamp-file nil)))            ;; No time stamp in the file
+;;(setq org-publish-project-alist
+;;      (list
+;;       (list "website-org-mode"
+;;             :recursive t
+;;             :base-directory "./content"
+;;             :publishing-function 'org-html-publish-to-html
+;;             :publishing-directory "./public"
+;;             :with-author nil                   ;; Don't show author
+;;             :with-creator t                    ;; Show emacs and org version
+;;             :with-toc nil                      ;; Don't show tables of content
+;;             :section-numbers nil               ;; No number section
+;;             :time-stamp-file nil)))            ;; No time stamp in the file
 
 ;; Generate the site output
-(org-publish-all t)
+;;(org-publish-all t)
 
 (message "Build complete!")
