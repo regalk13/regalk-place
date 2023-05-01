bf9d41d553d2fc171b2e02b807d8908870da65d7
regalk <jsalazarcaviedes30@gmail.com>
2023-04-30 21:29:32 -0500

    Converting my personal website

--- build-site.el
+++ build-site.el
@@ -19,8 +19,8 @@
 ;; Customize the HTML output
 (setq org-html-validation-link nil              ;; Don't show validation link
       org-html-head-include-scripts nil         ;; Use own scripts
-      org-html-head-include-default-style nil   ;; Use own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+      org-html-head-include-default-style nil   ;; Use own styles 
+      org-html-head "<link rel=\"stylesheet\" href=\"css/style.css\" /><link href=\"fonts/iosevka.css\" rel=\"stylesheet\"/>")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
@@ -32,7 +32,7 @@
              :publishing-directory "./public"
              :with-author nil                   ;; Don't show author
              :with-creator t                    ;; Show emacs and org version
-             :with-toc t                      ;; Don't show tables of content
+             :with-toc nil                      ;; Don't show tables of content
              :section-numbers nil               ;; No number section
              :time-stamp-file nil)))            ;; No time stamp in the file
 
--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+# Copy css files to public folder
+echo "Copying css files to public folder"
+cp -r content/css public/
+
+# Build site
 emacs -Q --script build-site.el
